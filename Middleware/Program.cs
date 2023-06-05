d30b7f4cdb5de9fc28f64f2a93f16de8d8d069f9
spox <82903@student.pb.edu.pl>
2023-06-05 21:52:32 +0200

    Redirecting page working

--- Middleware/Program.cs
+++ Middleware/Program.cs
@@ -1,3 +1,5 @@
+using Middleware.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,7 +14,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseMiddleware<RedirectPage>();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
---
+++ Middleware/Utils/RedirectPage.cs
@@ -0,0 +1,52 @@
+﻿using DeviceDetectorNET;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Threading.Tasks;
+
+namespace Middleware.Utils
+{
+    public class RedirectPage
+    {
+        private readonly RequestDelegate _next;
+
+        public RedirectPage(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string userAgent = context.Request.Headers["User-Agent"].ToString();
+            var userAgentParser = new DeviceDetector(userAgent);
+            userAgentParser.Parse();
+
+            string browserName = userAgentParser.GetClient().Match.Name;
+            if (IsSupportedBrowser(browserName))
+            {
+                string redirectUrl = GetRedirectUrl(browserName);
+                if (!string.IsNullOrEmpty(redirectUrl))
+                {
+                    context.Response.Redirect(redirectUrl);
+                    return;
+                }
+            }
+
+            await _next(context);
+        }
+
+        private bool IsSupportedBrowser(string browserName)
+        {
+            string[] supportedBrowsers = { "Microsoft Edge", "EdgeChromium", "IE"};
+            return supportedBrowsers.Contains(browserName);
+        }
+
+        private string GetRedirectUrl(string browserName)
+        {
+            if (browserName == "Microsoft Edge" || browserName == "EdgeChromium" || browserName == "IE")
+            {
+                return "https://www.mozilla.org/pl/firefox/new/";
+            }
+            return string.Empty;
+        }
+    }
+}
